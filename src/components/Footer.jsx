0ccb27dfb97b64e2ced7fcbfcf3c7923bb9ef27d
Surya Sunkari <suryasunkari@gmail.com>
2024-08-22 18:51:30 -0500

    changed some of the adrian

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,17 +8,13 @@ const Footer = () => {
       <hr className='border-slate-200' />
 
       <div className='footer-container'>
-        <p>
-          © 2023 <strong>Adrian Hajdin</strong>. All rights reserved.
-        </p>
-
-        <div className='flex gap-3 justify-center items-center'>
+        <div className='flex gap-6 justify-center items-center'>
           {socialLinks.map((link) => (
             <Link key={link.name} to={link.link} target='_blank'>
               <img
                 src={link.iconUrl}
                 alt={link.name}
-                className='w-6 h-6 object-contain'
+                className='w-10 h-10 object-contain'
               />
             </Link>
           ))}
