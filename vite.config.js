3d99fbae1bedcf07be270db9ffe2107ee50bb6e2
Surya Sunkari <suryasunkari@gmail.com>
2024-12-05 17:17:35 -0600

    testing new 404.html

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/personal-portfolio-three-js/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
+  },
   assetsInclude: ['**/*.glb'],
 })
