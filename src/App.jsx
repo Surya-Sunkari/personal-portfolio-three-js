013c03724ca736672d2a3fa9122d2ed60005b7f8
Surya Sunkari <suryasunkari@gmail.com>
2024-12-05 18:57:50 -0600

    removed baseurl, switched base in vite config to /

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { About, Contact, Home, Projects } from "./pages";
 const App = () => {
   return (
     <main className='bg-slate-300/20'>
-      <Router basename="/personal-portfolio-three-js/">
+      <Router>
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
